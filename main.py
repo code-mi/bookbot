6e7fbdd409acaeff48445a6ba42468ac49d23c6b
code-mi <weak.ocelot.vrpb+git@masked.net>
2026-01-07 13:32:20 +0100

    added main func to main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 def get_book_text(book_name):
     # getting file contents of the book
-    file_contents = f.read() book_name
-    pass
+    with open(book_name, "r") as f:
+        file_contents = f.read()
+    return file_contents
+
 
 def main():
+    book_text = get_book_text("books/frankenstein.txt")
+    print(book_text)
 
 
 main()
