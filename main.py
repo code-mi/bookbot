5db799142844b6b397904f5086004735c8221141
code-mi <weak.ocelot.vrpb+git@masked.net>
2026-01-09 11:03:44 +0100

    .gitignore changes, main.py new func, stats.py added

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from stats import count_char, count_words
+
+
 def get_book_text(book_name):
     # getting file contents of the book
     with open(book_name, "r") as f:
@@ -7,7 +10,14 @@ def get_book_text(book_name):
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
-    print(book_text)
+    # print(book_text)
+    # counts how many words are in the book and prints the result
+    word_count = count_words(book_text)
+    print(f"Found {word_count} total words.")
+
+    # counting the chars in the book and prints them in a dict
+    char_count = count_char(book_text)
+    print(f"{char_count}\n")
 
 
 main()
---
+++ stats.py
@@ -0,0 +1,9 @@
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+
+def count_char(text):
+    s = list(text)
+
+    return s
